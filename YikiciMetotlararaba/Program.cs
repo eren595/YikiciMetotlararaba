750daecff6cbe7d1f0827353ecd9e7624f3faf12
eren595 <eren595@BPC12>
2023-12-12 12:02:33 +0300

    Proje dosyası ekle.

---
+++ YikiciMetotlararaba/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YikiciMetotlararaba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program başladı.");
+            Otomobil oto = new Otomobil();
+            Console.WriteLine("Program bitti.");
+        }
+    }
+    class Otomobil
+    {
+        string marka = "";
+        string renk = "";
+        public Otomobil()
+        {
+            marka ="TOGG";
+            renk = "kırmızı";
+            Console.WriteLine("Yapıcı metot çalıştı.");
+        }
+        ~Otomobil()
+        {
+            Console.WriteLine("Nesne hafızadan atıldı.");
+        }
+    }
+
+
+}
+    
+
